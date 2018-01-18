5080b3e09ff921879325f8d4c7a480f9eb898dd0
Wanderson P. Barcelos <wp.barcelos@gmail.com>
2018-01-18 08:29:08 -0200

    grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,41 @@
 module.exports = function(grunt){
-
     'use strict';
-
 
     grunt.initConfig({
         autoprefixer:{
+            dist:{
+                files:{
+                    'deploy/css/main.css': 'source/sass/main.css',
+                }
+            }
 
         },
         copy:{
-
+            
         },
         clean:{
 
         },
         cssmin:{
+            dist:{
+                files:{
+                    'deploy/css/main.css':'deploy/css/main.css'
+                }
+            }
 
         },
         uglify:{
+            options:{
+                mangle:true
+            },
+            dist:{
+                files: {
+                    'deploy/javascript/app.min.js':[
+                        'source/javascript/incrementButton.js',
+                        'source/javascript/date.js'
+                    ]
+                }
+            }
 
         },
     });
@@ -25,8 +44,6 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
-
-
     
 }
 
